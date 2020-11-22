6d210f1dba7fd80b55a1ac0b3a1830b9b2dc481e
graudtV <55806873+graudtV@users.noreply.github.com>
2020-11-22 16:03:23 +0300

    add setlinebuf() macro

--- frequent_ipc.h
+++ frequent_ipc.h
@@ -4,6 +4,7 @@
 #define FREQUENT_IPC_H_
 
 #include <sys/types.h> // for key_t
+#include <stdio.h>
 
 /*  Describes individual semaphore in System V semaphore set
  *  Structure is used in functions like sem_inc(), sem_dec()
@@ -23,6 +24,10 @@ int sem_dec(semaphore_t sem);
 int sem_add(semaphore_t sem, unsigned int value);
 int sem_sub(semaphore_t sem, unsigned int value);
 int sem_add_signed(semaphore_t sem, int value);
+
+/*  There is no setlinebuf() in stdio.h according to standard.
+ * But Linux has it. So it is defined as a macro to avoid conflicts */
+#define setlinebuf(stream) setvbuf((stream), NULL, _IOLBF, 0)
 
 /*  If macro FREQUENT_FEATURES_SINGLE_FILE is defined,
  * source code of frequent_ipc.c will be included in place */
